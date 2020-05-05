b0451756ad1a9655e2f4085524ed68dd685fb197
imad <imad.live1990@gmail.com>
2020-05-05 03:42:05 +0200

    install bootstrap and jquery and add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CinemaComponent } from './cinema/cinema.component';
+import {RoutingModule} from './routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CinemaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RoutingModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
